88024d9896a1489e69b428330373c6289e015a3f
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-11-26 06:29:31 -0500

    Inital express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+  'mongodb://bob:Syn44mlab%24%24@ds141697.mlab.com:41697/devconnector'
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users.js');
+const profile = require('./routes/api/profile.js');
+const posts = require('./routes/api/posts.js');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
