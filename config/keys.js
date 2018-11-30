3e59c0aca8026da7005bc48c817d2d6d6e383884
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-11-30 16:48:25 -0500

    login feature initialized

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI:
-  'mongodb://bob:Syn44mlab%24%24@ds141697.mlab.com:41697/devconnector'
+  mongoURI: 'mongodb://bob:Syn44mlab%24%24@ds141697.mlab.com:41697/devconnector',
+  secretOrKey: 'secret'
 };
