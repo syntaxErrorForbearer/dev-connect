f5d9a4703958e3f821cc0e943e9a3b5447a89106
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-12-03 14:35:21 -0500

    Profile model creattion

---
+++ validation/is-empty.js
@@ -0,0 +1,10 @@
+// function isEmpty(value) {
+// module.exports = value =>
+// module.exports = const isEmpty = value =>
+const isEmpty = value =>
+    value === undefined ||
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
