81615ca58c2bc0de5ef118ac363e43b189311aee
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2019-01-19 12:41:36 -0500

    like and unlike - 27

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -66,30 +66,7 @@ router.post('/', passport.authenticate('jwt', { session: false }),
   }
 );
 
-/*
-// @route   DELETE api/posts/:id
-// @desc    Delete post
-// @access  Private
-router.delete('/:id', passport.authenticate(
-  'jwt', { session: false }), (req, res) => {
-    Profile.findOne({ user: req.user.id })
-      .then(profile => {
-        Post.findById(req.params.id)
-          .then(post => {
-            // Check for post owner
-            if (post.user.toString() !== req.user.id) {
-              return res
-                .status(401)
-                .json({ notauthorized: 'User not authorized' });
-            }
 
-            // Delete
-            post.remove().then(() => res.json({ success: true }));
-          })
-          .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
-      })
-});
-*/
 // @route   DELETE api/posts/:id
 // @desc    Delete post
 // @access  Private
@@ -110,6 +87,64 @@ router.delete(
           // Delete
           post.remove().then(() => res.json({ success: true }));
         })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found to delete' }));
+    });
+  }
+);
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ alreadyliked: 'User already liked this post' });
+          }
+
+          // Add user id to likes array
+          post.likes.unshift( { user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found to delete' }));
+    });
+  }
+);
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            if(post.likes.filter(
+              like => like.user.toString() === req.user.id)
+                .length = 0) { return res
+                  .status(400)
+                  .json({ notliked: 'You have not yet liked this post' });
+                }
+
+            // Get remove index
+            const removeIndex = post.likes
+              .map(item => item.user.toString())
+              .indexOf(req.user.id);
+
+            // Splice out of array
+            post.likes.splice(removeIndex, 1);
+
+            // Save
+            post.save().then(post => res.json(post));
+          })
         .catch(err => res.status(404).json({ postnotfound: 'No post found to delete' }));
     });
   }
