5a043c25d08a8ffd267ac82af5a3cdf00e929df0
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2019-02-03 05:00:23 -0500

    Profile reducer and Spinner - 48

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions'
 
 import { Provider } from 'react-redux'
 import store from './store'
@@ -10,9 +11,10 @@ import store from './store'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+
 
 import './App.css';
 
@@ -31,7 +33,8 @@ if(localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
 
-    // TODO: Clear current profiles
+    // Clear current profiles
+      store.dispatch(clearCurrentProfile());
 
     // Redirect to loginUser
     window.location.href = '/login';
@@ -50,6 +53,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
