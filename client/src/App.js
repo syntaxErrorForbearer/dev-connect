7804376076ccf071fcafaaf6ee3b1b35f83c8f51
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2019-02-03 00:12:20 -0500

    TextFieldGroupInput Component - 46

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import { Provider } from 'react-redux'
 import store from './store'
@@ -24,6 +24,19 @@ if(localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired Token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // TODO: Clear current profiles
+
+    // Redirect to loginUser
+    window.location.href = '/login';
+
+  }
 }
 
 class App extends Component {
